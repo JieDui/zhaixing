ccc3c8132b882eefc1641001742551680b7d6ded
hyh990511 <53004798+hyh990511@users.noreply.github.com>
2020-05-05 00:08:18 +0800

    完成卡片内网站操作接口

--- backend/src/main/java/com/example/catchstars/controller/WebSiteController.java
+++ backend/src/main/java/com/example/catchstars/controller/WebSiteController.java
@@ -45,4 +45,46 @@ public class WebSiteController {
             return JsonResult.forbidden("this card is not yours", null);
         }
     }
+
+    @ApiOperation(value = "将网站从卡片中移除", httpMethod = "DELETE")
+    @DeleteMapping("/starts/websites/delwebsites/")
+    public Object deleteWebsiteFromCard(@RequestParam int cardId, @RequestParam int websiteID){
+        if (session.getAttribute(SESSION_KEY) == null) {
+            return JsonResult.forbidden("please login first", null);
+        }
+        if(webSiteService.delAWebSiteFromCard(cardId,websiteID))
+            return JsonResult.ok("");
+        else return JsonResult.forbidden("this website is not exist", null);
+
+    }
+
+
+    @ApiOperation(value = "将网站添加进卡片中", httpMethod = "POST")
+    @PostMapping("/starts/websites/addwebsites/")
+    public Object addAWebSiteToCard(@RequestParam int cardId, @RequestParam int websiteID){
+        if (session.getAttribute(SESSION_KEY) == null) {
+            return JsonResult.forbidden("please login first", null);
+        }
+        if(webSiteService.addAWebSiteToCard(cardId,websiteID))
+            return JsonResult.ok("");
+        else return JsonResult.forbidden("this website is not exist", null);
+
+
+    }
+
+    @ApiOperation(value = "修改网站消息", httpMethod = "PUT")
+    @putMapping("/starts/websites/updatewebsites/")
+    public Object updateWebsiteByCard(@RequestBody Website website){
+        if (session.getAttribute(SESSION_KEY) == null) {
+            return JsonResult.forbidden("please login first", null);
+        }
+        if(webSiteService.modifyWebSite(website))
+            return JsonResult.ok("");
+        else return JsonResult.forbidden("this website is not exist", null);
+
+    }
+
+
+
+
 }
